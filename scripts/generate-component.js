0456bf3198e453d587c678b828e3bd8d17160f2c
Enzo Bozzani <bozzanienzo@gmail.com>
2024-09-03 15:49:57 -0300

    feat: add create-component script and install carbon and sass

---
+++ scripts/generate-component.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const component = (name) => `import styles from './${name}.module.scss';
+
+export interface ${name}Props {}
+
+export const ${name} = ({}: ${name}Props) => {
+  return <div>Hello 👋, I am a ${name} component.</div>;
+};
+`;
+
+const styles = `@use '@carbon/colors';
+@use '@carbon/layout';
+@use '@carbon/grid';
+`;
+
+const index = (name) => `import { ${name} } from './${name}';
+
+export default ${name}
+`;
+
+const [name] = process.argv.slice(2);
+if (!name) throw new Error('You must include a component name.');
+
+const dir = `./src/components/${name}/`;
+
+if (fs.existsSync(dir)) throw new Error('A component with that name already exists.');
+
+fs.mkdirSync(dir);
+
+function writeFileErrorHandler(err) {
+	if (err) throw err;
+}
+
+fs.writeFile(`${dir}/${name}.tsx`, component(name), writeFileErrorHandler);
+fs.writeFile(`${dir}/${name}.module.scss`, styles, writeFileErrorHandler);
+fs.writeFile(`${dir}/index.ts`, index(name), writeFileErrorHandler);
+
+console.log(`${name} component created successfully!`);
